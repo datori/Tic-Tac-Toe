f1390365336e578c15875bb0e9895a26fa7840cc
Daniel Serratore <daniel75027@gmail.com>
2017-05-31 13:48:33 -0500

    Added placeholder player selection, placeholder AI, refactored code.

--- index.js
+++ index.js
@@ -2,23 +2,54 @@ $(document).ready(function(){
 	game.start();
 });
 	
-var game = {
-	player: "X",
+const game = {
+	player: "",
+	AI: "",
 	winner: "",
 	board: ["","","","","","","","",""],
 	board_DOM: [$("#cell0"),$("#cell1"),$("#cell2"),$("#cell3"),$("#cell4"),$("#cell5"),$("#cell6"),$("#cell7"),$("#cell8")],
 	start: function(){
 		// Handle initial startup logic.
 		this.handlers();
+		this.player = prompt("X or O?");
+		switch(this.player){
+			case "X":
+				this.AI = "O";
+				break;
+			case "O":
+				this.AI = "X";
+		};
 	},
-	renderBoard: function(){
-		// Update board_DOM with contents of board.
-		for (var i=0; i<9; i++){
-			this.board_DOM[i].text(this.board[i]);
-		}
-		if (!this.gameState()){
-			alert(this.winner);
-		}
+	handlers: function(){
+		// Event handlers to drive the logic.
+		var game = this;
+		$(".cell").click(function(){
+			// Pass cell number to validation function.
+			var cell = $(this).attr("id").match(/\d/)[0];
+			if (game.moveValidate(cell) && game.gameState()){
+				game.board[cell] = game.player;
+				game.renderBoard();
+				game.AIMove();
+			}
+		});
+	},
+	AIMove: function(){
+		// Generate array of empty cells (possible moves).
+		// map() to return the index instead of the item itself.
+		// filter() to remove the undefined values, since map() will return undefined for filled cells.
+		var moves = this.board.map(function(item, index){
+			if (item == ""){
+				return index;
+			}
+		}).filter(function(cell){
+			if (cell){
+				return cell;
+			}
+		});
+		// Select cell to choose.
+		this.board[moves[Math.floor(Math.random() * moves.length)]] = this.AI;
+		this.renderBoard();
+
 	},
 	moveValidate: function(cell){
 		// Check if cell is already taken.
@@ -68,16 +99,13 @@ var game = {
 			return win;
 		}	
 	},
-	handlers: function(){
-		// Event handlers to drive the logic.
-		var game = this;
-		$(".cell").click(function(){
-			// Pass cell number to validation function.
-			var cell = $(this).attr("id").match(/\d/)[0];
-			if (game.moveValidate(cell) && game.gameState()){
-				game.board[cell] = game.player;
-				game.renderBoard();
-			}
-		});
-	},
+	renderBoard: function(){
+		// Update board_DOM with contents of board.
+		for (var i=0; i<9; i++){
+			this.board_DOM[i].text(this.board[i]);
+		}
+		if (!this.gameState()){
+			alert(this.winner);
+		}
+	}
 }
